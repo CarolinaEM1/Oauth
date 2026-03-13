6dfac2a741c901f9a0b43c8a44c0f8497f0feb52
CarolinaEM1 <20031299@itcelaya.edu.mx>
2026-03-12 20:25:20 -0600

    inicio de sesion

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+
+<h2>Iniciar sesión</h2>
+
+<a href="discord-login.php">
+<button>Login con Discord</button>
+</a>
+
+<br><br>
+
+<a href="spotify-login.php">
+<button>Login con Spotify</button>
+</a>
