201e01d0185442583fb7d87177b800c801acf839
CarolinaEM1 <20031299@itcelaya.edu.mx>
2026-03-12 20:24:13 -0600

    configuración

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+$discord_client_id = "TU_CLIENT_ID";
+$discord_client_secret = "TU_CLIENT_SECRET";
+$discord_redirect = "http://localhost/oauth-login-php/discord-callback.php";
+
+$spotify_client_id = "TU_CLIENT_ID";
+$spotify_client_secret = "TU_CLIENT_SECRET";
+$spotify_redirect = "http://localhost/oauth-login-php/spotify-callback.php";
+
+?>
